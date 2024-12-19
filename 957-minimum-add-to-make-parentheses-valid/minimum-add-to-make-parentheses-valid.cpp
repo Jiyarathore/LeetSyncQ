8de7263cebbcb12089da533581dcc979d8939c52
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2024-12-19 13:13:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (15.79%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+        int c=0;
+        for(char ch:s){
+            if(ch=='('){
+                st.push(ch);
+            }
+            else{
+               if(st.empty()) c++;
+               else st.pop();
+            }
+        }
+        return st.size() +c;
+    }
+};
